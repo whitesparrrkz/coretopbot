2e5b37555d278d205c18feb0bb95b1a8fa61461f
whitesparrrkz <daniel.sebastian.galvan@gmail.com>
2025-01-20 00:34:50 -0800

    added get & add commands

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 from dotenv import load_dotenv
 import os
 import discord
+from slash_commands.get_level import get_level_by_id, get_level_by_name 
+from slash_commands.add_level import add_level
 
 load_dotenv()
 Token: str = os.getenv("DISCORD_TOKEN")
 
 bot = discord.Bot()
 
-@bot.slash_command(name="get_level", description="Gets a level by list position")
-@discord.option("pos", type=discord.SlashCommandOptionType.integer)
-async def getLevel(ctx: discord.ApplicationContext, pos: int):
-    await ctx.respond(f"Not yet... u gave {pos}")
+guilds = [1330730208046743652]
+
+@bot.slash_command(guilds=guilds, name="get_level_by_id", description="Gets a level by list position")
+@discord.option("level_position", type=discord.SlashCommandOptionType.integer)
+async def get_level_by_id_cmd(ctx: discord.ApplicationContext, level_position: int):
+    embed = get_level_by_id(level_position)
+    if embed == None:
+        await ctx.respond(f"Invalid level position **( {level_position} )**")
+        return
+    await ctx.respond(embed=embed)
+
+@bot.slash_command(guilds=guilds, name="get_level_by_name", description="Gets a level by level name")
+@discord.option("level_name", type=discord.SlashCommandOptionType.string)
+async def get_level_by_name_cmd(ctx: discord.ApplicationContext, level_name: str):
+    embed = get_level_by_name(level_name)
+    if embed == None:
+        await ctx.respond(f"Invalid level name **( {level_name} )**")
+        return
+    await ctx.respond(embed=embed)
+
+@bot.slash_command(guilds=guilds, name="add_level", description="Adds a new level")
+async def add_level_cmd(ctx: discord.ApplicationContext):
+    modal = add_level()
+    await ctx.send_modal(modal=modal)
 
 def main() -> None:
     print("Bot is running.")
