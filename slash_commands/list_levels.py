c82c16a7fa15c1ff0a9dfc7743ac04c9d3354082
benn9909 <duy9909@gmail.com>
2025-01-25 21:02:52 +0700

    readded your code and made mine a comment

--- slash_commands/list_levels.py
+++ slash_commands/list_levels.py
@@ -41,11 +41,16 @@ def getLevels(pos: int):
 
 def makeEmbed(levels, pos):
     embed = discord.Embed(title="The CORETOP List", color=discord.Colour.blue())
-    
 
+    values=""
 
     for level in levels:
-        embed.add_field(name=f"**{str(level["level_position"])}. {level["level_name"]} - {level["level_creator"]}**\n", value=f"Victors: {level["get_victors"]}" inline=True)
+        values += f"**{str(level["level_position"])}.** `{level["level_name"]}`\n"
+
+    # for level in levels: 
+    # embed.add_field(name=f"**{str(level["level_position"])}. {level["level_name"]} - {level["level_creator"]}**\n", value=f"Victors: {level["get_victors"]}" inline=True)
+
+    embed.add_field(name="Levels:", value=values, inline=True)
 
     embed.set_footer(text=f"Showing levels {pos}-{pos+range-1}")
     return embed
